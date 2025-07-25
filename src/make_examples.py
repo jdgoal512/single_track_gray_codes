7bec8a007b27af9e6ad0fb315d6b8022b887c9db
jdgoal512 <jonathandgleason@gmail.com>
2025-07-25 12:28:08 -0600

    Added lookup tables

--- src/make_examples.py
+++ src/make_examples.py
@@ -23,15 +23,21 @@ def main():
     with open("examples/README.md", "w", encoding="utf-8") as md_file:
         md_file.write("# Example Single Track Gray Codes\n")
         for i, gray_codes in gray_codes_by_n_sensors.items():
-            md_file.write(f"## {i} Sensors\n")
-            md_file.write("| Visualization | Track Size | Resolution | Sensor Positions | Track | Position Lookup Table |\n")
+            if i == 1:
+                md_file.write("## 1 Sensor")
+            else:
+                md_file.write(f"## {i} Sensors\n")
+            md_file.write("| Visualization | Number of Positions | Resolution | Sensor Positions | Track | Position Lookup Table |\n")
             md_file.write("| ---- | ---- | ---- | ---- | ---- | ---- |\n")
             for (g, visualization) in gray_codes:
                 resolution = f"{360/g.n_track:0.2f}{DEGREE}"
                 sensors = ', '.join(str(i) for i in g.sensor_positions)
                 track = f"{g.track:0{g.n_track}b}"
                 track = "<br>".join([track[i:i+15] for i in range(0, len(track), 15)])  # Split long tracks into groups of 15 character lines
-                lookup = "TODO"
+                lookup_values = []
+                for j in range(g.n_track):
+                    lookup_values.append(f"{g.get_reading(j):0{g.n_sensors}b}")
+                lookup = f"[{', '.join(lookup_values)}]"
                 image = f'<img src="{visualization}" alt="Animation" width="400" height="400">'
                 md_file.write(f"| {image} | {g.n_track} | {resolution} | {sensors} | {track} | {lookup} |\n")
 
